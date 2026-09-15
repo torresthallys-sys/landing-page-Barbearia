fff85ad137e2f0746b555128a680942c4f823c80
torresthallys <torresthallys@gmail.com>
2026-09-15 10:42:01 -0300

    validaçao de comentario

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   server: {
     host: true,
-    port: 5173,
+    port: 5173, // porta frontend
     allowedHosts: [".monkeycode-ai.live"],
     proxy: {
       "/api": {
